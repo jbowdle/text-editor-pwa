9e6b71e06d7cb4f6f7f6c2f6d6ae64bf05468401
jbowdle <johnwbowdle@gmail.com>
2023-06-16 22:46:54 -0500

    added asset caching; fixed issue with database getDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,7 +7,7 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: 'id' });
       console.log('jate database created');
     },
   });
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ content: content });
+  const request = store.put({ content: content, id: 1 });
 
   const result = await request;
   console.log('Data saved to database', result);
@@ -33,8 +33,8 @@ export const getDb = async () => {
   const request = store.getAll();
 
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result[0].content);
+  return result[0].content;
 }
 
 initdb();
