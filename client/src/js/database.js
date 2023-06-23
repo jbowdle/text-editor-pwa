d443d620585eeb794657d5c1939af3e4ca73d34d
jbowdle <johnwbowdle@gmail.com>
2023-06-22 21:26:42 -0500

    edited comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,19 +12,20 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// adds content to database
 export const putDb = async (content) => {
   console.log('PUT content to database');
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ content: content, id: 1 });
+  // const request = store.put({ value: content, id: 1 });
 
   const result = await request;
   console.log('Data saved to database', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// retrieves data from database
 export const getDb = async () => {
   console.log('GET all content from database');
   const jateDB = await openDB('jate', 1);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,6 @@ let installPrompt = null;
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   installPrompt = event;
@@ -10,7 +9,6 @@ window.addEventListener('beforeinstallprompt', (event) => {
   butInstall.style.display = "inline-block";
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
   if (!installPrompt) {
     return;
@@ -26,7 +24,6 @@ butInstall.addEventListener('click', async () => {
   }
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   console.log("JATE has been installed");
 });
